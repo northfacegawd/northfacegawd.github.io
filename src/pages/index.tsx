cd964625ec81c0d6586898bc877e63ffc5d4d360
northfacegawd <chuck5732590@gmail.com>
2022-10-29 11:29:01 +0900

    feat: styled components 타입 추가 및 global css 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react';
-import Text from '@components/Text';
+import styled from 'styled-components';
+
+const MainLayout = styled.div`
+  max-width: 50rem;
+  margin: auto;
+  width: 100%;
+`;
 
 const IndexPage: FC = function () {
-  return <Text text="Home" />;
+  return <MainLayout></MainLayout>;
 };
 
 export default IndexPage;
